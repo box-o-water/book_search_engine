d8554a838155100766ef77dad28dab749b4e8dc6
box-o-water <boxooowater@gmail.com>
2023-04-12 15:29:01 -0500

    fix(login/signup): trying more stuff but still not working

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import {
   createHttpLink,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
@@ -42,13 +42,16 @@ function App() {
       <Router>
         <>
           <Navbar />
-          <Switch>
-            <Route exact path="/" component={SearchBooks} />
-            <Route exact path="/saved" component={SavedBooks} />
-            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
-          </Switch>
+          <Routes>
+            <Route path="/" element={<SearchBooks />} />
+            <Route path="/saved" element={<SavedBooks />} />
+            <Route
+              path="*"
+              element={<h1 className="display-2">Wrong page!</h1>}
+            />
+          </Routes>
         </>
-      </Router>
+      </Router>{" "}
     </ApolloProvider>
   );
 }
